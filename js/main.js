ac274665af0ea794507f015db5d55140cab9a650
Giovanni <giovannipor23@gmail.com>
2021-03-19 17:56:46 +0100

    Esercizio Base Completo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 var app = new Vue({
     el: "#root",
+    data: {
+        counter: 0,
+        pics: [
+            "img/sun1.jpg",
+            "img/sun2.jpg",
+            "img/sun3.jpg",
+            "img/sun4.jpg",
+            "img/sun5.jpg"
+        ]
+    },
+    methods: {
+        prPic: function() {
+            this.counter --;
+            if (this.counter < 0) this.counter = this.pics.length-1;
+        },
+        nxPic: function() {
+            this.counter ++;
+            if (this.counter == this.pics.length) this.counter = 0;
+        }
+    }
 });
